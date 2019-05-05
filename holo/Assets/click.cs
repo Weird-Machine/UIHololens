589ed0b024c17c84f1645eecb853a18d4b7acd6d
Giuseppe Modica <pino.modica@hotmail.it>
2019-05-05 14:01:36 +0200

    RipensamentoTOT
    Ripensamento totale dell'idea

---
+++ holo/Assets/click.cs
@@ -0,0 +1,28 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour
+{
+
+    private Color[] colors = new Color[] { Color.red, Color.cyan };
+    private int i = 0;
+    private Renderer rend;
+    // public GameObject cube2;
+    void Start()
+    {
+
+        rend = GetComponent<Renderer>();
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+
+        rend.material.color = colors[i];
+        i = (i + 1) % colors.Length;
+    }
+
+
+}
