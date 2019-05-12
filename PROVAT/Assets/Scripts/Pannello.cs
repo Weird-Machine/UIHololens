f31ddf1e8927740e94387743275117a31b2c7144
Giuseppe Modica <pino.modica@hotmail.it>
2019-05-12 21:20:16 +0200

    update
    update

---
+++ PROVAT/Assets/Scripts/Pannello.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pannello : MonoBehaviour
+{
+    bool AriaCheked = false;
+    bool CentralinaCheked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckAria()
+    {
+        if (CentralinaCheked)
+        {
+            AriaCheked = true;
+            Debug.Log("Pannello: Aria");
+            this.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    public void CheckCentralina()
+    {
+        if (!CentralinaCheked)
+        {
+            CentralinaCheked = true;
+            Debug.Log("Pannello: Centralina");
+            this.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
